df5ffcc1d5f9f8e6fbaeb6f223389d78517718e1
ZhangYoufa <207986989@qq.com>
2020-08-30 23:01:28 +0800

    jpa中，表控制/字段控制注解的使用

--- src/main/java/com/youyou/demo/model/Banner.java
+++ src/main/java/com/youyou/demo/model/Banner.java
@@ -1,14 +1,19 @@
 package com.youyou.demo.model;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
 
 @Entity
+@Table(name = "banner1")
 public class Banner {
     @Id
     private long id;
+    @Column(length = 16)
     private String name;
+
+    @Transient
     private String description;
     private String img;
     private String title;
