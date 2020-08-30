5dd672b7b0776f2c2b8068f0064c71159eea8a3a
ZhangYoufa <207986989@qq.com>
2020-08-30 22:49:36 +0800

    在配置文件里面对jpa进行配置 添加jdbc数据库连接配置 使用jpa标签注解 在pom.xml文件中添加依赖

--- src/main/java/com/youyou/demo/model/Banner.java
+++ src/main/java/com/youyou/demo/model/Banner.java
@@ -1,6 +1,12 @@
 package com.youyou.demo.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Banner {
+    @Id
     private long id;
     private String name;
     private String description;
