bd946fc63f310d9a1cdf9e5a7ceb051a40348822
ZhangYoufa <207986989@qq.com>
2020-09-06 15:45:20 +0800

    从数据库表生成的模型 SET/GET使用@Getter@Setter注解代替 成员变量的类型换成Java使用的类型 添加导航属性

--- src/main/java/com/youyou/demo/model/Banner.java
+++ src/main/java/com/youyou/demo/model/Banner.java
@@ -1,120 +1,29 @@
 package com.youyou.demo.model;
 
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
 public class Banner {
-    private int id;
+    @Id
+    private Long id;
     private String name;
     private String description;
-    private Timestamp createTime;
-    private Timestamp updateTime;
-    private Timestamp deleteTime;
+    private Date createTime;
+    private Date updateTime;
+    private Date deleteTime;
     private String title;
     private String img;
 
-    @Id
-    @Column(name = "id", table = "banner")
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Basic
-    @Column(name = "name", table = "banner")
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Basic
-    @Column(name = "description", table = "banner")
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Basic
-    @Column(name = "create_time", table = "banner")
-    public Timestamp getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Timestamp createTime) {
-        this.createTime = createTime;
-    }
-
-    @Basic
-    @Column(name = "update_time", table = "banner")
-    public Timestamp getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Timestamp updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    @Basic
-    @Column(name = "delete_time", table = "banner")
-    public Timestamp getDeleteTime() {
-        return deleteTime;
-    }
-
-    public void setDeleteTime(Timestamp deleteTime) {
-        this.deleteTime = deleteTime;
-    }
-
-    @Basic
-    @Column(name = "title", table = "banner")
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    @Basic
-    @Column(name = "img", table = "banner")
-    public String getImg() {
-        return img;
-    }
-
-    public void setImg(String img) {
-        this.img = img;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Banner banner = (Banner) o;
-        return id == banner.id &&
-                Objects.equals(name, banner.name) &&
-                Objects.equals(description, banner.description) &&
-                Objects.equals(createTime, banner.createTime) &&
-                Objects.equals(updateTime, banner.updateTime) &&
-                Objects.equals(deleteTime, banner.deleteTime) &&
-                Objects.equals(title, banner.title) &&
-                Objects.equals(img, banner.img);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, description, createTime, updateTime, deleteTime, title, img);
-    }
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bannerId")
+    protected List<BannerItem> bannerItems;
 }
--- src/main/java/com/youyou/demo/model/BannerItem.java
+++ src/main/java/com/youyou/demo/model/BannerItem.java
@@ -1,12 +1,18 @@
 package com.youyou.demo.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.sql.Timestamp;
 import java.util.Objects;
 
 @Entity
 @Table(name = "banner_item", schema = "sleeve", catalog = "")
+@Getter
+@Setter
 public class BannerItem {
+    @Id
     private int id;
     private String img;
     private String keyword;
@@ -17,114 +23,4 @@ public class BannerItem {
     private int bannerId;
     private String name;
 
-    @Id
-    @Column(name = "id")
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Basic
-    @Column(name = "img")
-    public String getImg() {
-        return img;
-    }
-
-    public void setImg(String img) {
-        this.img = img;
-    }
-
-    @Basic
-    @Column(name = "keyword")
-    public String getKeyword() {
-        return keyword;
-    }
-
-    public void setKeyword(String keyword) {
-        this.keyword = keyword;
-    }
-
-    @Basic
-    @Column(name = "type")
-    public short getType() {
-        return type;
-    }
-
-    public void setType(short type) {
-        this.type = type;
-    }
-
-    @Basic
-    @Column(name = "create_time")
-    public Timestamp getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Timestamp createTime) {
-        this.createTime = createTime;
-    }
-
-    @Basic
-    @Column(name = "update_time")
-    public Timestamp getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Timestamp updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    @Basic
-    @Column(name = "delete_time")
-    public Timestamp getDeleteTime() {
-        return deleteTime;
-    }
-
-    public void setDeleteTime(Timestamp deleteTime) {
-        this.deleteTime = deleteTime;
-    }
-
-    @Basic
-    @Column(name = "banner_id")
-    public int getBannerId() {
-        return bannerId;
-    }
-
-    public void setBannerId(int bannerId) {
-        this.bannerId = bannerId;
-    }
-
-    @Basic
-    @Column(name = "name")
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        BannerItem that = (BannerItem) o;
-        return id == that.id &&
-                type == that.type &&
-                bannerId == that.bannerId &&
-                Objects.equals(img, that.img) &&
-                Objects.equals(keyword, that.keyword) &&
-                Objects.equals(createTime, that.createTime) &&
-                Objects.equals(updateTime, that.updateTime) &&
-                Objects.equals(deleteTime, that.deleteTime) &&
-                Objects.equals(name, that.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, img, keyword, type, createTime, updateTime, deleteTime, bannerId, name);
-    }
 }
