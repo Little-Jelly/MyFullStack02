698c2e83b3da786fd58febe76e0cbb3dbdea8f9e
ZhangYoufa <207986989@qq.com>
2020-09-15 22:40:29 +0800

    解决实体类中的被提取到基类中的字段无法被返回到前端的问题

--- src/main/java/com/youyou/demo/model/BaseEntity.java
+++ src/main/java/com/youyou/demo/model/BaseEntity.java
@@ -3,10 +3,12 @@ package com.youyou.demo.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.MappedSuperclass;
 import java.util.Date;
 
 @Getter
 @Setter
+@MappedSuperclass
 public abstract class BaseEntity {
     private Date createTime;
     private Date updateTime;
