489a78d71ceb6475a4d61b2a8ff6fce2d376cfac
ZhangYoufa <207986989@qq.com>
2020-09-17 21:24:57 +0800

    给SPU新建一个视图

---
+++ src/main/java/com/youyou/demo/vo/SpuSimplifyVO.java
@@ -0,0 +1,13 @@
+package com.youyou.demo.vo;
+
+public class SpuSimplifyVO {
+    private Long id;
+    private String title;
+    private String subtitle;
+    private String img;
+    private String forThemeImg;
+    private String price;
+    private String discountPrice;
+    private String description;
+    private Integer sketchSpecId;
+}
