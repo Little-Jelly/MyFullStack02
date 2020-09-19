cfeeee2004f592e9ced58cf76e1e540fbc096da2
ZhangYoufa <207986989@qq.com>
2020-09-19 23:50:17 +0800

    编写获取分类SPU的接口

--- src/main/java/com/youyou/demo/api/v1/SpuController.java
+++ src/main/java/com/youyou/demo/api/v1/SpuController.java
@@ -54,4 +54,13 @@ public class SpuController {
         Page<Spu> page = this.spuService.getLaestPagingSpu(pageCounter.getPage(), pageCounter.getCount());
         return new PagingDozer<Spu, SpuSimplifyVO>(page, SpuSimplifyVO.class);
     }
+
+    @GetMapping("/by/category/{id}")
+    public PagingDozer<Spu, SpuSimplifyVO> getByCategoryId(@PathVariable(name = "id") Long id,
+                                                           @RequestParam(name = "is_root") Boolean isRoot,
+                                                           @RequestParam(name = "start", defaultValue = "0") Integer start,
+                                                           @RequestParam(name = "count", defaultValue= "10") Integer count){
+        
+        return null;
+    }
 }
