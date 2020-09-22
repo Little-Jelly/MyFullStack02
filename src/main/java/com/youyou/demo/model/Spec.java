5ba91ac6bbeecd5c898e784e1f96aec02f23696d
youyouba <207986989@qq.com>
2020-09-22 15:39:38 +0800

    新建Spec模型

---
+++ src/main/java/com/youyou/demo/model/Spec.java
@@ -0,0 +1,13 @@
+package com.youyou.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Spec {
+    private Long keyId;
+    private String key;
+    private Long valueId;
+    private String value;
+}
